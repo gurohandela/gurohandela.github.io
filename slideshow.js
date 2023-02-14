a13858134ed65d6bffe789d6c3516a9212704d6c
gurohandela <guro@handeland.info>
2023-02-14 19:22:34 +0100

    Made slideshow

---
+++ slideshow.js
@@ -0,0 +1,31 @@
+var slideIndex = 1;
+window.onload = function() {
+    plusDivs(1);
+}
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+/*function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+        x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 2000); //change every 2 sec.}
+    */
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("slides");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length };
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
