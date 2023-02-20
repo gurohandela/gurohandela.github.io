eeb4d8f2a6f4156408a87f26c8af9f2f0928c03a
gurohandela <guro@handeland.info>
2023-02-20 13:16:34 +0100

    Messy Slideshow

--- slideshow.js
+++ slideshow.js
@@ -1,10 +1,42 @@
-var slideIndex = 1;
-window.onload = function() {
-    plusDivs(1);
+var slideIndex = [1, 1];
+var slideId = ["slideshow1", "slideshow2"];
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, now) {
+    showSlides(slideshow.currentSlideIndex += n, now);
 }
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
+
+/*
+window.onload = loadit();
+
+function loadit() {
+    var slideshow1 = document.getElementById("slideshow1");
+    slideshow1.currentSlideIndex = 1;
+    showSlides(slideshow1.currentSlideIndex, slideshow1);
+
+    var slideshow2 = document.getElementById("slideshow2");
+    slideshow2.currentSlideIndex = 1;
+    showSlides(slideshow2.currentSlideIndex, slideshow2);
+}
+*/
+
+
+function plusSlides(n) {
+    showSlides(slideshow.currentSlideIndex += n, slideshow);
+}
+
+function showSlides(n, now) {
+    var i;
+    var x = slideshow.getElementsByClassName(slideId[now]);
+    if (typeof slideIndex[no] === 'undefined') { slideIndex[no] = 1; } //showslides corrects itself
+    if (n > x.length) {slideIndex[now] = 1}
+    if (n < 1) {slideIndex[now] = x.length}
+    for (i = 0; i < slideNow.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideId[now]-1].style.display = "block";
 }
 
 /*function carousel() {
@@ -18,7 +50,7 @@ function plusDivs(n) {
         x[slideIndex-1].style.display = "block";
     setTimeout(carousel, 2000); //change every 2 sec.}
     */
-
+/*
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("slides");
@@ -29,8 +61,4 @@ function showDivs(n) {
     }
     x[slideIndex - 1].style.display = "block";
 }
-
-function fillCanvas(img) {
-    var canvas = document.getElementsByClassName("canvas_test");
-    
-}
+*/
