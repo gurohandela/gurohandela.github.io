8cbe0b8688309b95af254712f69e52e110519acc
gurohandela <guro@handeland.info>
2023-02-20 09:42:15 +0100

    Home Menu

---
+++ home.js
@@ -0,0 +1,19 @@
+function openHome() {
+    window.location='index.html';
+
+}
+
+function openAbout() {
+    window.location='about.html';
+}
+
+function openPhoto() {
+    window.location = 'photo.html';
+
+}
+
+function openProjects() {
+    window.location = 'projects.html';
+}
+
+
