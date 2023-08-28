79c1bd59d908bd610b0bcb59aa2a7acefbe65a9d
gurohandela <guro@handeland.info>
2023-08-28 14:09:33 +0200

    added functional react

---
+++ my-react-version/src/App.js
@@ -0,0 +1,16 @@
+import './styles.css';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <Home />
+            <Footer/>
+        </>
+    )
+}
+
+export default App;
---
+++ my-react-version/src/components/Where.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Where() {
+  return (
+    <div className="grid"> 
+        <div className="where">
+            <h2>Oslo, Oslo |<b>||</b>|21.08.2023</h2>
+        </div>
+    <canvas className="whitespace" id="ws_w here"></canvas>
+    </div>
+  )
+}
+
+export default Where
